96b78d1dce3e70abb22e2a60663bbc2c97e98a83
cris1972 <cris72@mail.ru>
2021-06-14 10:46:43 -0700

    create some fragments

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -2,10 +2,40 @@ package com.rsschool.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.rsschool.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnStart.setOnClickListener {
+
+        }
+       var viewPeger= binding.viewPeger
+        viewPeger.adapter=ViewPegerAdapter(supportFragmentManager)
+
+        
+    }
+    inner class ViewPegerAdapter(fm : FragmentManager):FragmentPagerAdapter(fm)
+    {
+        override fun getCount(): Int {
+          return 5
+        }
+
+        override fun getItem(position: Int): Fragment {
+
+
+
+        }
+
     }
 }
